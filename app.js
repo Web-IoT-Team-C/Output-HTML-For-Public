d7bd1c6e0057f3d3361c53ded4275e6e30f0965e
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2020-12-03 19:29:50 +0900

    グラフをセットする関数を実装

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
-function test(){
-    alert("危険");
+var testData = {
+    二酸化炭素: 32,
+    マスク率: 73,
+    湿度: 53,
 }
+
+function main(){var num = 1;
+    for(i in testData){
+        var ctx = document.getElementById("chart"+num++);
+        showChart(ctx, i, testData[i])
+    }
+}
+
+
+function showChart(ctx, title, data){
+    var color;
+
+    if(data <= 33){
+        color = "#00fa9a"
+    }else if(data <= 66){
+        color = "#ffd700"
+    }else{
+        color = "#a52a2a"
+    }
+
+    var chart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            datasets: [{
+                backgroundColor: [
+                    color
+                ],
+                data: [data, 100-data]
+            }]
+          },
+          options: {
+            title: {
+              display: true,
+              text: title
+            }
+          }
+    });
+}
+
+main();
