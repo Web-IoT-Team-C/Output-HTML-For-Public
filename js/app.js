4e403964120d6f75ceeed051015995be7b464160
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2020-12-08 23:26:36 +0900

    基礎組作成

--- js/app.js
+++ js/app.js
@@ -1,66 +1,28 @@
-var testData = 70
-var testPer = {co2:"20", humidity:"56", temperture:"23"}
-
-var w = $('.chart-box').width();
-var h = $('.chart-box').height();
-$('#chart').attr('width', w);
-$('#chart').attr('height', h);
-const ctx = document.getElementById("chart");
-
-const co2 = document.getElementById("co2");
-const humidity = document.getElementById("humidity");
-const temperature = document.getElementById("temperature");
-
-function main(){
-    co2.innerHTML = testPer["co2"] + "ppm"
-    humidity.innerHTML = testPer["humidity"] + "%"
-    temperature.innerHTML = testPer["temperture"] + "°"
-
-    showChart(ctx, "総合危険度", testData)
-}
-
-
-function showChart(ctx, title, data){
-    var color;
-
-    if(data <= 33){
-        color = "#00fa9a"
-    }else if(data <= 66){
-        color = "#ffd700"
-    }else{
-        color = "#a52a2a"
-    }
-
-      // グラフオプション
-  var options = {
-    // グラフの太さ（中央部分を何％切り取るか）
-    cutoutPercentage: 65,
-    // 凡例を表示しない
-    legend: { display: false },
-    // 自動サイズ変更をしない
-    responsive: true,
-    // タイトル
-    title: {
-      display: true,
-      fontSize: 40,
-      text: title,
+new Chart(document.getElementById("chart"), {
+    type: "horizontalBar",
+    data: {
+      labels: ["Foo"],
+      datasets: [
+        {data: [45], backgroundColor: "rgba(244, 143, 177, 0.6)" },
+        {data: [55], backgroundColor: "rgba(100, 181, 246, 0.6)" }
+      ]
     },
-    // マウスオーバー時に情報を表示しない
-    tooltips: { enabled: false },
-  };
-
-    var chart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            datasets: [{
-                backgroundColor: [
-                    color
-                ],
-                data: [data, 100-data]
-            }]
-          },
-          options: options
-    });
-}
-
-main();
+    options: {
+        responsive: false,
+        legend: {
+            position: 'bottom'
+        },                
+        scales: {
+            xAxes: [
+                {
+                    stacked: true,
+                }
+            ],
+            yAxes: [
+                {
+                    stacked: true,
+                }
+            ]
+        }
+    }
+  });
--- js/plugin.js
+++ js/plugin.js
@@ -1,31 +1,153 @@
-var chartJsPluginCenterLabel = {
-  afterDatasetsDraw: function (chart) {
-    // ラベルの X 座標と Y 座標
-    var canvas = chart.ctx.canvas;
-    var labelX = canvas.clientWidth / 2;
-    var labelY = Math.round((canvas.clientHeight + chart.chartArea.top) / 2);
-    // ラベルの値
-    var value = chart.data.datasets[0].data[0] + '%';
-    // ラベル描画
-    var ctx = this.setTextStyle(chart.ctx);
-    ctx.fillText(value, labelX, labelY);
-  },
-
-  /**
-    * 書式設定
-    */
-  setTextStyle: function (ctx) {
-    var fontSize = 60;
-    var fontStyle = 'normal';
-    var fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif';
-    ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
-    ctx.fillStyle = '#636363';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-
-    return ctx;
+(function(Chart) {
+  var isObject = function(obj) {
+    var type = typeof obj;
+    return type === 'object' && !!obj;
   }
-};
 
-// 上記プラグインの有効化
-Chart.plugins.register(chartJsPluginCenterLabel);
+  var dataValue = function(dataPoint, isHorizontal) {
+    if (isObject(dataPoint)) {
+      return isHorizontal ? dataPoint.x : dataPoint.y;
+    }
+
+    return dataPoint;
+  }
+
+  var cloneArray = function(srcAry) {
+    var dstAry = [];
+    var length = srcAry.length;
+
+    for (var i = 0; i < length; i++) {
+      dstAry.push(srcAry[i]);
+    }
+    return dstAry;
+  };
+
+  var setOriginalData = function(data) {
+    data.originalData = data.datasets.map(function(dataset) {
+      return cloneArray(dataset.data);
+    });
+  };
+
+  // set calculated rate (xx%) to data.calculatedData
+  var calculateRate = function(data, isHorizontal, precision) {
+    var visibles = data.datasets.map(function(dataset) {
+      if (!dataset._meta) return true;
+
+      for (var i in dataset._meta) {
+        return !dataset._meta[i].hidden;
+      }
+    });
+    
+    var datasetDataLength = 0;
+    if (data && data.datasets && data.datasets[0] && data.datasets[0].data) {
+      datasetDataLength = data.datasets[0].data.length;
+    }
+    var totals = Array.apply(null, new Array(datasetDataLength)).map(function(el, i) {
+      return data.datasets.reduce(function(sum, dataset, j) {
+        var key = dataset.stack;
+        if (!sum[key]) sum[key] = 0;
+        sum[key] += Math.abs(dataValue(dataset.data[i], isHorizontal)) * visibles[j];
+
+        return sum;
+      }, {});
+    });
+
+    data.calculatedData = data.datasets.map(function(dataset, i) {
+      return dataset.data.map(function(val, i) {
+        var total = totals[i][dataset.stack];
+        var dv = dataValue(val, isHorizontal);
+        return dv && total ? round(dv / total, precision) : 0;
+      });
+    });
+  };
+
+  var getPrecision = function(pluginOptions) {
+    // return the (validated) configured precision from pluginOptions or default 1
+    var defaultPrecision = 1;
+    if (!pluginOptions.hasOwnProperty("precision")) return defaultPrecision;
+    if (!pluginOptions.precision) return defaultPrecision;
+    var optionsPrecision = Math.floor(pluginOptions.precision);
+    if (isNaN(optionsPrecision)) return defaultPrecision;
+    if (optionsPrecision < 0 || optionsPrecision > 16) return defaultPrecision; 
+    return optionsPrecision;
+  };
+
+  var round = function(value, precision) {
+    var multiplicator = Math.pow(10, precision);
+    return Math.round(value * 100 * multiplicator) / multiplicator;
+  };
+
+  var tooltipLabel = function(isHorizontal) {
+    return function(tooltipItem, data) {
+      var datasetIndex = tooltipItem.datasetIndex;
+      var index = tooltipItem.index;
+      var datasetLabel = data.datasets[datasetIndex].label || "";
+      var originalValue = data.originalData[datasetIndex][index];
+      var rateValue = data.calculatedData[datasetIndex][index];
+
+      return "" + datasetLabel + ": " + rateValue + "% (" + dataValue(originalValue, isHorizontal) + ")";
+    }
+  };
+
+  var reflectData = function(srcData, datasets) {
+    if (!srcData) return;
+
+    srcData.forEach(function(data, i) {
+      datasets[i].data = data;
+    });
+  };
+
+  var isHorizontalChart = function(chartInstance) {
+    return chartInstance.config.type === "horizontalBar";
+  }
+
+  var Stacked100Plugin = {
+    id: "stacked100",
+
+    beforeInit: function(chartInstance, pluginOptions) {
+      if (!pluginOptions.enable) return;
+
+      var xAxes = chartInstance.options.scales.xAxes;
+      var yAxes = chartInstance.options.scales.yAxes;
+      var isVertical = chartInstance.config.type === "bar" || chartInstance.config.type === "line";
+
+      [xAxes, yAxes].forEach(function(axes) {
+        axes.forEach(function(hash) {
+          hash.stacked = true;
+        });
+      });
+      (isVertical ? yAxes : xAxes).forEach(function(hash) {
+        if (!hash.ticks.min) {
+          var hasNegative = chartInstance.data.datasets.some(function(dataset) {
+            return dataset.data.some(function(value) {
+              return value < 0;
+            });
+          });
+          hash.ticks.min = hasNegative ? -100 : 0;
+        }
+        if (!hash.ticks.max) hash.ticks.max = 100;
+      });
+
+      // Replace tooltips
+      if (pluginOptions.hasOwnProperty("replaceTooltipLabel") && !pluginOptions.replaceTooltipLabel) return;
+      chartInstance.options.tooltips.callbacks.label = tooltipLabel(isHorizontalChart(chartInstance));
+    },
+
+    beforeDatasetsUpdate: function(chartInstance, pluginOptions) {
+      if (!pluginOptions.enable) return;
+
+      setOriginalData(chartInstance.data);
+      var precision = getPrecision(pluginOptions);
+      calculateRate(chartInstance.data, isHorizontalChart(chartInstance), precision);
+      reflectData(chartInstance.data.calculatedData, chartInstance.data.datasets);
+    },
+
+    afterDatasetsUpdate: function(chartInstance, pluginOptions) {
+      if (!pluginOptions.enable) return;
+
+      reflectData(chartInstance.data.originalData, chartInstance.data.datasets);
+    }
+  };
+
+  Chart.pluginService.register(Stacked100Plugin);
+}.call(this, Chart));
